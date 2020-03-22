8e2b3eefe1098112b8215542f048368fb8cd9e48
elisemayo <49425910+elisemayo@users.noreply.github.com>
2020-03-21 21:08:00 -0700

    main

---
+++ Assignment 2/main.py
@@ -0,0 +1,29 @@
+# Elise May
+# Student ID: 2271041
+# Email: may137@mail.chapman.edu
+# CPSC 408
+# Assignment 2
+
+from DatabaseCRUD import CRUD
+from Menu import Menu
+
+if __name__ == "__main__":
+    crud = CRUD("C:\\Users\\elise may\\Documents\\CPSC_Courses\\CPSC408\\Assignment 2\\StudentDB.sqlite")
+    menu = Menu()
+
+    while True:
+        menu.printMenu()
+        action = menu.choose()
+
+        if action == 1:
+            crud.display()
+        elif action == 2:
+            crud.create()
+        # elif action == 3:
+        #     crud.update()
+        # elif action == 4:
+        #     crud.delete()
+        # elif action == 5:
+        #     crud.search()
+        elif action == 6:
+            break
